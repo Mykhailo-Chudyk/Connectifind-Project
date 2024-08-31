9c4aba28b794ad849a9df4fb9794671d7f5a0af9
mchudyk <mykhailo.m.chudyk@gmail.com>
2024-08-31 12:17:30 +0200

    Create table for feed post and run initial migration

--- backend/feed_posts/models.py
+++ backend/feed_posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import uuid
+from users.models import User  
+from events.models import Event  
 
-# Create your models here.
+class FeedPost(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    eventId = models.ForeignKey(Event, on_delete=models.CASCADE, verbose_name="Event")
+    authorId = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Author")
+    content = models.TextField()
+    time = models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self):
+        return f"Post by {self.authorId} for event {self.eventId}"
