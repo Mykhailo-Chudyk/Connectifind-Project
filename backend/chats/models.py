a306a862b4a94d744c6308d418fd5f5c4d237f0d
mchudyk <mykhailo.m.chudyk@gmail.com>
2024-08-31 11:48:53 +0200

    Create table for chats and run initial migration

--- backend/chats/models.py
+++ backend/chats/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import uuid
+from users.models import User 
 
-# Create your models here.
+class Chat(models.Model):
+    chatId = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    authorId1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='chat_user1')
+    authorId2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='chat_user2')
+
+    def __str__(self):
+        return f"{self.authorId1} and {self.authorId2}"
