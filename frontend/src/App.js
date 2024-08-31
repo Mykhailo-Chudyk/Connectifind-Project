7b9b2f5fd182474ceebb8443f25e52c0b04fad97
mchudyk <mykhailo.m.chudyk@gmail.com>
2024-08-31 11:15:58 +0200

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import HelloWorld from './components/Delete';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+
+  return (
+    <Router>
+      <Routes> 
+        <Route path="/" element={<>Home</>} />
+        <Route path="/login" element={<>Login</>} />
+        <Route path="/signup" element={<>Sign Up</>} />
+        <Route path="/events" element={<>Events</>} />
+        <Route path="/add-event" element={<>Add Event</>} />
+        <Route path="/event/:eventId" element={<>Event</>} />
+        <Route path="/me/profile" element={<>Default profile</>} />
+        <Route path="/me/settings" element={<>Settings</>} />
+        <Route path="/event/:eventId/me" element={<>Event customized profile</>} />
+        <Route path="/event/:eventId/feed" element={<>Event feed</>} />
+        <Route path="/event/:eventId/people" element={<>All the people who attend the event</>} />
+        <Route path="/event/:eventId/people/:userId" element={<>A user who attends event</>} />
+        <Route path="/event/:eventId/chats" element={<>All the chats user has in a specific event</>} /> 
+        <Route path="/event/:eventId/about" element={<>About the event</>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Delete.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+// An example component that fetches a message from the backend
+function HelloWorld() {
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/user/hello/')
+            .then(response => {
+                setMessage(response.data.message);
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
+    }, []); 
+
+    return (
+        <div>
+            <h1>{message}</h1>
+        </div>
+    );
+}
+
+export default HelloWorld;
