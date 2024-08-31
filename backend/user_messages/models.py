f8a988384c677c069e6017afc60bb653c9b0ae9b
mchudyk <mykhailo.m.chudyk@gmail.com>
2024-08-31 11:58:04 +0200

    Create table for messages and run initial migration

--- backend/user_messages/models.py
+++ backend/user_messages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import uuid
+from users.models import User
+from chats.models import Chat
 
-# Create your models here.
+class Message(models.Model):
+    messageId = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    chatId = models.ForeignKey(Chat, on_delete=models.CASCADE, verbose_name="Chat")
+    authorId = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Author")
+    time = models.DateTimeField(auto_now_add=True) 
+    content = models.TextField()
+
+    def __str__(self):
+        return f"Message by {self.authorId} in {self.chatId}"
