ee2c5688ed4245572a1e71f6fba7ec02f949ff70
mchudyk <mykhailo.m.chudyk@gmail.com>
2024-09-28 14:40:58 +0200

    Connect chat to a specific event

--- backend/chats/models.py
+++ backend/chats/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 import uuid
-from users.models import User 
+from users.models import User
+from events.models import Event
 
 class Chat(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     authorId1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='chat_user1')
     authorId2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='chat_user2')
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='chats')
 
     def __str__(self):
-        return f"{self.authorId1} and {self.authorId2}"
+        return f"Chat between {self.authorId1} and {self.authorId2} for Event {self.event.title}"
