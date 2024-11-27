617005ffc85b3085705d07bccb59ef63b3c657da
Mykhailo Chudyk <mykhailochudyk@Mykhailos-MacBook-Air.local>
2024-11-26 16:56:25 -0800

    Redirect people to / page by default

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -7,15 +7,24 @@ import "./styles.scss";
 
 const Header = ({ isAuthenticated }) => {
   const { logout } = useContext(AuthContext);
+
   const navigate = useNavigate();
+
+  console.log(isAuthenticated);
+
   return (
     <header>
       <div className="header-container">
-        <h2>ConnectiFind</h2>
+        <h2 onClick={() => navigate('/')}>ConnectiFind</h2>
         {isAuthenticated ?
-        <div>
-
-        </div> :
+        <div className="header-buttons"> 
+          <ButtonComponent text="Browse Events" level="primary" onClick={() => {
+            navigate('/events');
+          }}/>
+          <ButtonComponent text="Create" level="secondary" onClick={() => {
+            navigate('/add-event');
+          }}/>
+        </div>:
         <div className="header-buttons"> 
           <ButtonComponent text="Login" level="primary" onClick={() => {
             navigate('/login');
@@ -25,21 +34,6 @@ const Header = ({ isAuthenticated }) => {
           }}/>
         </div>}
       </div>
-      {/* {isAuthenticated ? (
-        <nav>
-          <p>Welcome, User!</p>
-          <a href="/events">Events</a>
-          <a href="/me/profile">Profile</a>
-          <a href="/me/settings">Settings</a>
-          <button onClick={logout}>Logout</button>
-        </nav>
-      ) : (
-        <nav>
-          <p>Welcome, Guest!</p>
-          <a href="/login">Login</a>
-          <a href="/signup">Sign Up</a>
-        </nav>
-      )} */}
     </header>
   );
 };
