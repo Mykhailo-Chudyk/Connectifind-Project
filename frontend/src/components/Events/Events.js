c24fb1cc30d0ba6ac7bccc056dc73b463aeba90d
Mykhailo Chudyk <mykhailochudyk@Mykhailos-MacBook-Air.local>
2025-01-08 17:57:42 +0100

    Add skeletons to /events page

--- frontend/src/components/Events/Events.js
+++ frontend/src/components/Events/Events.js
@@ -3,11 +3,14 @@ import eventservice from '../../services/eventservice.js';
 import ButtonComponent from '../ButtonComponent/ButtonComponent.js';
 import { useNavigate } from 'react-router-dom';
 import { FaCalendarAlt } from "react-icons/fa";
-import './styles.scss'; 
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+import './styles.scss';
 
 const Events = () => {
   const [events, setEvents] = useState([]);
   const [error, setError] = useState('');
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -18,12 +21,29 @@ const Events = () => {
       } catch (err) {
         setError('Failed to fetch events');
         console.error(err);
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchEvents();
   }, []);
 
+  const EventSkeleton = () => (
+    <div className='event-item'>
+      <div className='event-item-image'>
+        <Skeleton height={200} />
+      </div>
+      <div className='event-item-body'>
+        <Skeleton width={150} height={20} className='event-author' />
+        <Skeleton width={200} height={16} className='event-title' />
+        <Skeleton count={1} height={16} className='event-description' />
+      </div>
+      <div className='event-item-actions'>
+        <Skeleton width={180} height={20} className='event-time-location' />
+      </div>
+    </div>
+  );
 
   return (
     <div className='events-container'>
@@ -33,24 +53,30 @@ const Events = () => {
       </div>
       {error && <p>{error}</p>}
       <div className='events-list'>
-        {events.map((event) => (
-          <div key={event.id} onClick={() => navigate(`${event.id}`)} className='event-item'>
-            <div className='event-item-image'>
-              {event?.image && <img src={event?.image} alt={event.title} />}
-              {!event?.image && <FaCalendarAlt />}
+        {loading ? (
+          Array(9).fill().map((_, index) => (
+            <EventSkeleton key={index} />
+          ))
+        ) : (
+          events.map((event) => (
+            <div key={event.id} onClick={() => navigate(`${event.id}`)} className='event-item'>
+              <div className='event-item-image'>
+                {event?.image && <img src={event?.image} alt={event.title} />}
+                {!event?.image && <FaCalendarAlt />}
+              </div>
+              <div className='event-item-body'>
+                <p className='event-author'>{event.author.first_name} {event.author.last_name}</p>
+                <p className='event-title'>{event.title}</p>
+                <p className='event-description'>{event.description}</p>
+              </div>
+              <div className='event-item-actions'>
+                <p className='event-time-location'>{event.location}</p>
+                <p className='event-time-location'>{new Date(event.time).toLocaleString()}</p>
+                <ButtonComponent text="Details" level="secondary" onClick={() => {navigate(`${event.id}`)}} />
+              </div>
             </div>
-            <div className='event-item-body'>
-              <p className='event-author'>{event.author.first_name} {event.author.last_name}</p>
-              <p className='event-title'>{event.title}</p>
-              <p className='event-description'>{event.description}</p>
-            </div>
-            <div className='event-item-actions'>
-              <p className='event-time-location'>{event.location}</p>
-              <p className='event-time-location'>{new Date(event.time).toLocaleString()}</p>
-              <ButtonComponent text="Details" level="secondary" onClick={() => {navigate(`${event.id}`)}} />
-            </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
