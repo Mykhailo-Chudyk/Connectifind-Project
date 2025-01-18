11a4b6b33bde79c10f283c4d68dcf67d933c2e29
Mykhailo Chudyk <mykhailochudyk@Mykhailos-MacBook-Air.local>
2025-01-18 15:03:48 -0800

    Make the process of the sidebar appearing and dispappering smoother

--- frontend/src/Layout.js
+++ frontend/src/Layout.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { AuthContext } from './AuthContext';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header/Header';
@@ -8,14 +8,39 @@ import { useSelector } from 'react-redux';
 const Layout = () => {
   const { isAuthenticated, logout } = useContext(AuthContext);
   const user = useSelector((state) => state.user.user);
+  const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    // Set a minimum loading time to prevent flashing
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 300);
+
+    return () => clearTimeout(timer);
+  }, [user]);
 
   return (
     <div>
       <Header isAuthenticated={isAuthenticated} />
       <div style={{ display: 'flex' }}>
-        {user && <Sidebar onLogout={logout} />}
-        <div style={{ width: '100%', height: '100%', marginLeft: user ? '75px' : '0px', marginTop: '86px' }}>
+        <div 
+          style={{ 
+            width: '75px',
+            transition: 'opacity 0.3s ease-in-out',
+            opacity: isLoading ? 0 : 1
+          }}
+        >
+          {(isAuthenticated && user) && <Sidebar onLogout={logout} />}
+        </div>
+        <div 
+          style={{ 
+            width: '100%', 
+            height: '100%', 
+            marginLeft: (isAuthenticated && user) ? '0px' : '0px', 
+            marginTop: '86px',
+            transition: 'margin-left 0.3s ease-in-out'
+          }}
+        >
           <Outlet />
         </div>
       </div>
