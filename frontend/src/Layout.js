fc2092f118221978d33fd764260edcaf373a1f0c
Mykhailo Chudyk <mykhailochudyk@Mykhailos-MacBook-Air.local>
2025-02-01 12:45:54 -0800

    Get navigated to top page when navigating between pages

--- frontend/src/Layout.js
+++ frontend/src/Layout.js
@@ -4,11 +4,13 @@ import { Outlet } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import { useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 
 const Layout = () => {
   const { isAuthenticated, logout } = useContext(AuthContext);
   const user = useSelector((state) => state.user.user);
   const [isLoading, setIsLoading] = useState(true);
+  const location = useLocation();
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -17,6 +19,10 @@ const Layout = () => {
 
     return () => clearTimeout(timer);
   }, [user]);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
 
   if (isLoading) {
     return (
