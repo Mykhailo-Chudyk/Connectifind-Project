e202118b24fca0b049b4bc147b9ee566c1547fcc
Mykhailo Chudyk <mykhailochudyk@Mykhailos-MacBook-Air.local>
2025-02-28 19:05:03 -0800

    Clean public folder and insert logo

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -6,7 +6,7 @@ import IconComponent from '../IconComponent/IconComponent.js';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { FaSearch } from 'react-icons/fa';
-import logo from '../../assets/connectifind.png';
+import logo from '../../assets/connectifind-logo.png';
 import useDeviceType from '../../hooks/useDeviceType';
 import "./styles.scss";
 
@@ -28,8 +28,8 @@ const Header = ({ isAuthenticated }) => {
   return (
     <header>
       <div className={`header-container`}>
-        <h3 onClick={() => navigate('/')} style={{cursor: "pointer"}}>ConnectiFind</h3>
-        {/* <img className="logo" src={logo} alt="ConnectiFind" onClick={() => navigate('/')} style={{cursor: "pointer"}}/> */}
+        {/* <h3 onClick={() => navigate('/')} style={{cursor: "pointer"}}>ConnectiFind</h3> */}
+        <img className="logo" src={logo} alt="ConnectiFind" onClick={() => navigate('/')} style={{cursor: "pointer",}}/>
         {!isMobile ? <div className="search-container">
           <form onSubmit={handleSearch}>
             <input
